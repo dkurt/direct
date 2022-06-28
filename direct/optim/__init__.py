53ad2f19099cd3a7170d817fa1498e1a55890013
georgeyiasemis <georgeyiasemis@hotmail.com>
2022-06-28 07:01:36 +0200

    Add shampoo

---
+++ direct/optim/__init__.py
@@ -0,0 +1,6 @@
+# coding=utf-8
+# Copyright (c) DIRECT Contributors
+
+from torch.optim import LBFGS, SGD, Adadelta, Adagrad, Adam, RMSprop, SparseAdam
+
+from direct.optim.shampoo import Shampoo
--- direct/types.py
+++ direct/types.py
@@ -1,7 +1,8 @@
 # coding=utf-8
 # Copyright (c) DIRECT Contributors
+
 import pathlib
-from typing import NewType, Union
+from typing import Any, Callable, Dict, Iterable, NewType, Optional, Tuple, Union
 
 import torch
 from torch import nn as nn
@@ -11,3 +12,7 @@ Number = Union[float, int]
 PathOrString = Union[pathlib.Path, str]
 FileOrUrl = NewType("FileOrUrl", PathOrString)
 HasStateDict = Union[nn.Module, torch.optim.Optimizer, GradScaler]
+LossClosure = Callable[[], float]
+OptLossClosure = Optional[LossClosure]
+OptFloat = Optional[float]
+Params = Union[Iterable[torch.Tensor], Iterable[Dict[str, Any]]]
